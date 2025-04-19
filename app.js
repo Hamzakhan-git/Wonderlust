1cb25f6ef3a7d09db413080d57381cd44b18e962
Hamzakhan-git <hamzakhan91827@gmail.com>
2025-04-19 18:37:18 +0530

    create boilerplate

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 main().then(()=> {
     console.log("connected to DB ");
 })
