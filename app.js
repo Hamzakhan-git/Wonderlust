2544bbc418b4abdc23349b799106e9c135602a89
Hamzakhan-git <hamzakhan91827@gmail.com>
2025-05-23 19:50:31 +0530

    Configuring strategy and create signup user

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const methodOverride = require("method-override");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const reviews = require("./routes/review.js");
-const listings = require("./routes/listing.js");
-const { log } = require("console");
+const reviewRouter = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const userRouter = require("./routes/user.js");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
 app.set("view engine", "ejs");
@@ -39,14 +42,31 @@ const sessionOptions ={
         httpOnly: true,
     }
 }
+
+//passport hashing algorithm = pbkdf2
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     console.log(res.locals.success);
     next();
-})
+});
+
+// app.get("/demouser", async(req,res) =>{
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student",
+//     });
+// let registeredUser = await User.register(fakeUser, "helloworld");
+// res.send(registeredUser);
+// })
 
 app.get("/",(req,res) => {
     res.send("Hi, I'm root");
@@ -54,8 +74,9 @@ app.get("/",(req,res) => {
 
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews",reviews)
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/", userRouter);
 
 app.all(/.*/, (req,res,next) =>{
     next(new ExpressError(404, "Page not Found!"));
